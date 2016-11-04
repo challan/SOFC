7a53b494add88c5e9da73e6e390d245f31120117
challan <challan@cepheus.engin.umich.edu>
2016-11-04 12:36:44 -0400

    Modified 3D_KKS_SrO.f90 (161104_A) 1. Modified the governing equations and parameters (Refer to 11/4/16 notebook)

--- 3D_KKS_SrO.f90
+++ 3D_KKS_SrO.f90
@@ -18,24 +18,23 @@ integer,parameter        :: nx=50, ny=50, nz=30
 ! Spatial and time-stepping sizes
 real(kind=8),parameter :: dt=.01d0,dx=1.d0,dy=1.d0,dz=1.d0
 ! Interfacial width controlling parameters
-real(kind=8),parameter :: eps2=2.d0,W=4.d0,Conc_ini=0.41d0
+real(kind=8),parameter :: eps2=2.d0,W=4.d0,Conc_ini=0.15d0
 ! Chemical and interfacial kinetic mobilities
 ! M_s is surface mobility Sr (for the growth of SrO on the Sr-rich surface)
-! M_b is bulk mobility of Sr in the bulk LSCF
+! D_b is diffusion coefficient of Sr in the bulk LSCF
 ! L_phi is interface kinetic mobility
-real(kind=8),parameter :: M_s=0.1d0,M_b=.01d0,L_phi=1.0d0
+real(kind=8),parameter :: M_s=0.1d0,D_b=.01d0,L_phi=1.0d0
 ! Bu stands for the bulk LSCF phase 
 ! Su stands for the Sr-rich surface phase
 ! Sr stands for SrO oxide phase
 !Coefficients to the free-energy curves in the form of A1(c-Cm)^2+A0
-real(kind=8),parameter :: A1Bu=1.d0, CmBu=0.4d0, A0Bu=0.1d0
-real(kind=8),parameter :: A1Su=1.d0, CmSu=0.5d0, A0Su=0.1d0
-real(kind=8),parameter :: A1Sr=1.d0, CmSr=0.d0, A0Sr=0.0d0
+real(kind=8),parameter :: A1Su=1.d0, CmSu=0.13d0, A0Su=0.1d0
+real(kind=8),parameter :: A1Sr=1.d0, CmSr=0.5d0, A0Sr=0.0d0
 
 ! I/O Variables
-integer,parameter        :: it_st=1, it_md=100000,it_ed=500000, it_mod=50000
+integer,parameter        :: it_st=1, it_md=50000,it_ed=500000, it_mod=50000
 character(len=100), parameter :: s = "SrO_on_LSCF"
-character(len=10), parameter :: dates="161102_A"
+character(len=10), parameter :: dates="161104_A"
 
 end module simulation
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -45,67 +44,53 @@ program KKS_3D
 use simulation
 implicit none
 
-real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) :: psi, Conc, div
-real(kind=8), DIMENSION(0:nx+1,0:ny+1) :: phi, SurfaceConc, SurfaceConc_old, SurfaceDiv, SurfaceDiv_AllPositive, Phi_Pot
+real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) :: Conc, div
+real(kind=8), DIMENSION(0:nx+1,0:ny+1) :: phi, SurfaceConc, SurfaceDiv, Phi_Pot
 
 real(kind=8):: tolerance, max_c, min_c, max_phi, min_phi
 integer:: iter,i,j,k
 
-integer, dimension(:), allocatable :: a, b
+	iter=0
+  	call initial_conds(Conc,phi)
+	call write_output(Conc,phi,iter)
+do iter=it_st,it_md
 
- 	iter=1
-  	call initial_conds(psi,Conc,phi,SurfaceConc)
-
-	write(*,*) 'Done equilibrating the concentration field @ iter=', iter-1
-	write(*,*) "Total Conc Value =",sum(Conc(1:nx,1:ny,1:nz)) 	
-	call write_output(Conc,SurfaceConc,iter-1)
-	
-write(*,*) "!!!!!!!!!!!! Begin Iteration !!!!!!!!!!!!!!"
-	
-do iter=1,it_md
-
-	!!Apply Periodic BC for the surface concentration
-	call boundary_conds_2D(SurfaceConc)	
-	! Divergence of grad mu on the surface
-	call calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv,SurfaceDiv_AllPositive)
-	SurfaceConc=SurfaceConc+dt*SurfaceDiv
-	
 	!!Apply Periodic BC for the bulk concentration
 	call boundary_conds_3D(Conc)
-	! Divergence of grad mu on the bulk
-	call calculate_divergence_bulk(psi,Conc,div)
+
+	!Update the surface concentration field
+	SurfaceConc(:,:)=Conc(:,:,nz)
 	
-	div(:,:,nz)=div(:,:,nz)+SurfaceDiv_AllPositive(:,:)
+	! Divergence of grad mu on the surface
+	call calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv) 
+
+	! Divergence of grad mu on the bulk
+	call calculate_divergence_bulk(Conc,SurfaceDiv,div)
   	!! Diffusion Iteration
-    Conc=Conc+dt*(div)
-		
+    Conc=Conc+dt*div
+	
+	
 	if (mod(iter,it_mod) .eq. 0) then	
-		call write_output(Conc,SurfaceConc,iter)	
+		call write_output(Conc,phi,iter)
 	endif
+
 enddo
 
-	write(*,*) 'Done equilibrating the concentration field @ iter=', iter-1
-!	write(*,*) "Total Conc Value =",sum(Conc(1:nx,1:ny,1:nz)) 	
-!	call write_output(Conc,SurfaceConc,iter-1)
 		
 do iter=it_md+1,it_ed
 	
 	!Update the surface concentration field
-	!SurfaceConc(:,:)=Conc(:,:,nz)
+	SurfaceConc(:,:)=Conc(:,:,nz)
 	
 	!!Apply Periodic BC for the surface concentration
 	call boundary_conds_2D(SurfaceConc)	
 	!!Apply Periodic BC for the structural parameter
 	call boundary_conds_2D(phi)
 
-	! Make sure both phi iteration uses SurfaceConc at the current time step
-	SurfaceConc_old=SurfaceConc
-
 	! Divergence of grad mu on the surface
-	call calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv,SurfaceDiv_AllPositive)
-    SurfaceConc=SurfaceConc+dt*SurfaceDiv
+	call calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv)
     
-	call calculate_potential_surface(phi,SurfaceConc_old,Phi_Pot)	
+	call calculate_potential_surface(phi,SurfaceConc,Phi_Pot)	
 	!!Apply Periodic BC for chemical potential for the Allen-Cahn eqn.
 	call boundary_conds_2D(Phi_Pot)
 	!! Allen-Cahn Iteration
@@ -114,14 +99,13 @@ do iter=it_md+1,it_ed
 	!!Apply Periodic BC for the bulk concentration
 	call boundary_conds_3D(Conc)
 	! Divergence of grad mu on the bulk
-	call calculate_divergence_bulk(psi,Conc,div)
-    div(:,:,nz)=div(:,:,nz)+SurfaceDiv_AllPositive(:,:)        
+	call calculate_divergence_bulk(Conc,SurfaceDiv,div)
   	!! Diffusion Iteration
     Conc=Conc+dt*div
 	
 	
 	if (mod(iter,it_mod) .eq. 0) then	
-		call write_output(Conc,SurfaceConc,iter)
+		call write_output(Conc,phi,iter)
 	endif
 
 enddo
@@ -130,55 +114,26 @@ write(*,*) "!!!!!!!!!!!! END Iteration !!!!!!!!!!!!!!"
 end program
 !*********************************************************************
 !*********************************************************************
-! Thermodynamic variables defined in bulk+Sr-rich surface phases.
-subroutine calculate_functions_bulk(Conc,psi,Hfunc_psi,ConcB,ConcS,dG_dCB)
-use simulation
-implicit none
-
-	real(kind=8), intent(in) ::Conc(0:nx+1,0:ny+1,0:nz+1)
-	real(kind=8), intent(in) ::psi(0:nx+1,0:ny+1,0:nz+1)
-
-	real(kind=8), intent(out) ::Hfunc_psi(0:nx+1,0:ny+1,0:nz+1)
-	real(kind=8), intent(out) ::ConcB(0:nx+1,0:ny+1,0:nz+1)
-	real(kind=8), intent(out) ::ConcS(0:nx+1,0:ny+1,0:nz+1)
-	real(kind=8), intent(out) ::dG_dCB(0:nx+1,0:ny+1,0:nz+1)
-		
-	! Monotonically increasing function	
-	Hfunc_psi=(psi**2.d0)*(3.0d0-2.d0*psi)
-
-    ! Auxillary bulk LSCF phase composition
-    ConcB=A1Su*(Conc-CmSu*Hfunc_psi)+A1Bu*CmBu*Hfunc_psi
-    ConcB=ConcB/(A1Bu*Hfunc_psi+A1Su*(1.0d0-Hfunc_psi))
-	! Auxillary Sr-rich surface phase composition
-    ConcS=A1Bu*(Conc-CmBu*(1.0d0-Hfunc_psi))+A1Su*CmSu*(1.0d0-Hfunc_psi)
-    ConcS=ConcS/(A1Bu*Hfunc_psi+A1Su*(1-Hfunc_psi))
-
-	! Derivative of the Free-energy function for the bulk LSCF phase
-	dG_dCB=2.0d0*A1Bu*(ConcB-CmBu)
-	
-end subroutine
-!*********************************************************************
-!*********************************************************************
-subroutine calculate_divergence_bulk(psi,Conc,div)
+subroutine calculate_divergence_bulk(Conc,SurfaceDiv,div)
 use simulation
 implicit none
 ! Iterate the diffusion equation in terms of the gradient of the chemical potential (derivative of the free-energy curve).
-	real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) ::psi, Conc, Chem_Mob, div
-	real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) ::Hfunc_psi, ConcB, ConcS, dG_dCB
-
+	real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) :: Conc, div
+	real(kind=8), DIMENSION(0:nx+1,0:ny+1) :: SurfaceDiv
 	integer:: i,j,k
-	
-	call calculate_functions_bulk(Conc,psi,Hfunc_psi,ConcB,ConcS,dG_dCB)
-	
-	!The diffusivity dependent mobility term smoothly varies between M_b and M_s across the interfacial region with the interpolation function h(psi)
- 	Chem_Mob=Hfunc_psi*M_s+(1.d0-Hfunc_psi)*M_b
-		
-	!!Divergence of M(phi) * grad(df_dc)
-    forall(i=1:nx,j=1:ny,k=1:nz)
-    	div(i,j,k)=((Chem_Mob(i,j,k)+Chem_Mob(i+1,j,k))*(dG_dCB(i+1,j,k)-dG_dCB(i,j,k))-(Chem_Mob(i,j,k)+Chem_Mob(i-1,j,k))*(dG_dCB(i,j,k)-dG_dCB(i-1,j,k))) / (2.d0*dx*dx) + &
-    			 ((Chem_Mob(i,j,k)+Chem_Mob(i,j+1,k))*(dG_dCB(i,j+1,k)-dG_dCB(i,j,k))-(Chem_Mob(i,j,k)+Chem_Mob(i,j-1,k))*(dG_dCB(i,j,k)-dG_dCB(i,j-1,k))) / (2.d0*dy*dy) + &
-    			 ((Chem_Mob(i,j,k)+Chem_Mob(i,j,k+1))*(dG_dCB(i,j,k+1)-dG_dCB(i,j,k))-(Chem_Mob(i,j,k)+Chem_Mob(i,j,k-1))*(dG_dCB(i,j,k)-dG_dCB(i,j,k-1))) / (2.d0*dz*dz)
+				
+	!! Fickian Diffusion in the bulk LSCF (minus the surface where the SrO forms)
+    forall(i=1:nx,j=1:ny,k=1:nz-1)
+    	div(i,j,k)=D_b*(Conc(i+1,j,k)+Conc(i-1,j,k)-2.d0*Conc(i,j,k)/dx**2.d0 + &
+    					Conc(i,j+1,k)+Conc(i,j-1,k)-2.d0*Conc(i,j,k)/dy**2.d0 + &
+    					Conc(i,j,k+1)+Conc(i,j,k-1)-2.d0*Conc(i,j,k)/dy**2.d0)
     end forall
+    
+    !! Combination of Fickian diffusion in the z-direction and diffusion via chem. pot. gradient determined by the KKS model at the surface
+    forall(i=1:nx,j=1:ny,k=nz:nz)
+    	div(i,j,k)=D_b*(Conc(i,j,k+1)+Conc(i,j,k-1)-2.d0*Conc(i,j,k)/dy**2.d0)
+    end forall
+    	div(:,:,nz)=div(:,:,nz)+SurfaceDiv(:,:)
 
 end subroutine
 !*********************************************************************
@@ -189,7 +144,6 @@ use simulation
 implicit none
 	real(kind=8), intent(in) ::SurfaceConc(0:nx+1,0:ny+1)
 	real(kind=8), intent(in) ::phi(0:nx+1,0:ny+1)
-
 	real(kind=8), intent(out) ::Hfunc_phi(0:nx+1,0:ny+1)
 	real(kind=8), intent(out) ::SurfaceConcSr(0:nx+1,0:ny+1)
 	real(kind=8), intent(out) ::SurfaceConcSu(0:nx+1,0:ny+1)
@@ -226,11 +180,11 @@ end subroutine
 !*********************************************************************
 !*********************************************************************
 ! Diffusion equation for Sr concentration on the Sr-rich surface where the SrO oxide grows
-subroutine calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv,SurfaceDiv_AllPositive)
+subroutine calculate_divergence_surface(phi,SurfaceConc,SurfaceDiv)
 use simulation
 implicit none
 ! Calculate the divergence of the gradient of the chemical potential (derivative of the free-energy curve).
-	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::phi, SurfaceConc, Chem_Mob, SurfaceDiv, SurfaceDiv_AllPositive
+	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::phi, SurfaceConc, Chem_Mob, SurfaceDiv
 
 	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::Hfunc_phi, SurfaceConcSr, SurfaceConcSu, GSr, GSu
 	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::dG_dCSu, dgdphi, dHfunc_dphi
@@ -243,16 +197,10 @@ implicit none
  	Chem_Mob=M_s
 			
 	!!Divergence of M(phi) * grad(df_dc)
-    do i=1,nx
-    do j=1,nx
+    forall(i=1:nx,j=1:ny)
     	SurfaceDiv(i,j)=((Chem_Mob(i,j)+Chem_Mob(i+1,j))*(dG_dCSu(i+1,j)-dG_dCSu(i,j))-(Chem_Mob(i,j)+Chem_Mob(i-1,j))*(dG_dCSu(i,j)-dG_dCSu(i-1,j))) / (2.d0*dx*dx) + &
     			 ((Chem_Mob(i,j)+Chem_Mob(i,j+1))*(dG_dCSu(i,j+1)-dG_dCSu(i,j))-(Chem_Mob(i,j)+Chem_Mob(i,j-1))*(dG_dCSu(i,j)-dG_dCSu(i,j-1))) / (2.d0*dy*dy)
-    	SurfaceDiv_AllPositive(i,j)=SurfaceDiv(i,j)
-    	if (SurfaceDiv_AllPositive(i,j) > 0.d0) then 
-    		SurfaceDiv_AllPositive(i,j) =0.d0
-    	endif
-    enddo
-    enddo
+    end forall
 
 end subroutine
 !*********************************************************************
@@ -276,9 +224,7 @@ implicit none
  			+dHfunc_dphi(i,j)*(GSr(i,j)-GSu(i,j)-(SurfaceConcSr(i,j)-SurfaceConcSu(i,j))*dG_dCSu(i,j))
    	end forall
 
-
 end subroutine
-
 !*********************************************************************
 !*********************************************************************
 subroutine boundary_conds_3D(phi)
@@ -313,31 +259,24 @@ implicit none
 end subroutine
 !*********************************************************************
 !*********************************************************************
-subroutine initial_conds(psi,Conc,phi,SurfaceConc)
+subroutine initial_conds(Conc,phi)
 use simulation
 implicit none
 
-	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1,0:nz+1) :: Conc,psi
-	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1) :: phi,SurfaceConc
+	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1,0:nz+1) :: Conc
+	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1) :: phi
 	
 	real(kind=8) :: x_coord, y_coord, xcenter, ycenter, radius, delta, circle
 	
 	integer :: i,j
 	
-!Domain parameter assumes a value of 1 in the Sr-rich surface phase and 0 in the bulk LSCF phase
-	psi(:,:,:)=0.d0
-	psi(:,:,nz:nz+1)=1.d0
-
 !The concentration in bulk	
 	Conc(:,:,:)=Conc_ini
 
-!The concentration in surface
-    SurfaceConc(:,:)=Conc_ini  		
-	
 !Structural parameter on the surface assumes a value of 1 in the SrO phase and 0 on the Sr-rich surface phase
 	xcenter=real(nx/2,kind=dbl)
 	ycenter=real(ny/2,kind=dbl)
-	radius=5.d0
+	radius=7.d0
 	delta=2.d0
 	do i=1,nx
 		x_coord=real(i,kind=dbl)
@@ -351,11 +290,11 @@ implicit none
 end subroutine
 !*********************************************************************
 !********************************************************************* 	
-subroutine write_output(Conc,SurfaceConc,iter)
+subroutine write_output(Conc,phi,iter)
 use simulation
 implicit none
 	real(kind=8), DIMENSION(0:nx+1,0:ny+1,0:nz+1) ::Conc
-	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::SurfaceConc
+	real(kind=8), DIMENSION(0:nx+1,0:ny+1) ::phi
 	INTEGER :: iter
 	CHARACTER(LEN=100) :: filename
 	CHARACTER(LEN=10) :: iteration
@@ -365,8 +304,8 @@ implicit none
 	write(*,*) "Total Conc Value =",sum(Conc(1:nx,1:ny,1:nz)) 	
 	write(*,*) "Maximum Value of Conc=", MAXVAL(Conc) 
 	write(*,*) "Minimum Value of Conc=", MINVAL(Conc) 	
- 	write(*,*) "Maximum Value of SurfaceConc=", MAXVAL(SurfaceConc) 
-	write(*,*) "Minimum Value of SurfaceConc=", MINVAL(SurfaceConc) 
+ 	write(*,*) "Maximum Value of phi=", MAXVAL(phi) 
+	write(*,*) "Minimum Value of phi=", MINVAL(phi) 
 	
 	if (iter < 10) then
 		format_string="(i1)"	
@@ -395,10 +334,10 @@ implicit none
 	write(1) Conc(1:nx,1:ny,1:nz)
 	close(1)	
 ! 
-	filename='data/'//trim(s)//'/'//trim(dates)//'/'//trim(s)//'_SurfaceConc_t'//trim(iteration)//'_'//trim(dates)//'.dat'
+	filename='data/'//trim(s)//'/'//trim(dates)//'/'//trim(s)//'_phi_t'//trim(iteration)//'_'//trim(dates)//'.dat'
 	write(*,*) filename
 	open(2,file=filename,form='unformatted',STATUS='REPLACE',ACTION='READWRITE')
-	write(2) SurfaceConc(1:nx,1:ny)
+	write(2) phi(1:nx,1:ny)
 	close(2)
 
 	
