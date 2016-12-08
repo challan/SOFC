cf2bd41d6dcd8114def1158ba9bccde3dff55211
challan <challan@cepheus.engin.umich.edu>
2016-12-08 14:55:39 -0500

    Modified 2D_SrO_Growth.f90 (161207_A) 1. Implemented correct BC between the substrate/particle interface. 2. Results in particle growth on top of the substrate (but it wets) 3. Sharp interface between the substrate and particle

--- 2D_SrO_Growth.f90
+++ 2D_SrO_Growth.f90
@@ -12,19 +12,19 @@ INTEGER, PARAMETER :: DBL = SELECTED_REAL_KIND (p=13)      ! Double data kind
 ! System Size
 integer,parameter        :: nx=100,ny=100
 ! Spatial and time-stepping sizes
-real(kind=8),parameter :: dt=.001d0,dx=1.d0,dy=1.d0
+real(kind=8),parameter :: dt=.005d0,dx=1.d0,dy=1.d0
 ! Interfacial width controlling parameters
 real(kind=8),parameter :: eps2=4.0d0,W=8.0d0
 ! Coefficients to the free-energy curves in the form of A1(c-Cm)^2+A0
 real(kind=8),parameter :: A1Al=1.d0, CmAl=0.08d0, A0Al=0.0d0
-real(kind=8),parameter :: A1Bt=1.d0, CmBt=.82d0, A0Bt=0.0d0
+real(kind=8),parameter :: A1Bt=1.d0, CmBt=.9d0, A0Bt=0.0d0
 !Equilibrium concentration of Sr in each phase
 real(kind=dbl),parameter :: CSr_s=.08d0, CSr_p=0.9d0, CSr_v=0.d0
 
 ! I/O Variables
-integer,parameter        :: it_st=1, it_ed=50000, it_mod=10000
+integer,parameter        :: it_st=1, it_ed=10000, it_mod=10000
 character(len=100), parameter :: s = "SrO_on_LSCF"
-character(len=10), parameter :: dates="161206_A"
+character(len=10), parameter :: dates="161207_A"
 
 end module simulation
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -41,25 +41,26 @@ integer:: iter,i,j,k
 
 	iter=0
 	call initial_conds(phi_Dom1,Conc,Conc_Dom1,Conc_Dom2)
-	call write_output(Conc_Dom1,Conc_Dom2,iter)
+	call write_output(Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2,iter)
 	
 do iter=it_st,it_ed
-
+	!write(*,*) "iter =",iter
 	!!Apply No-Flux BC for the concentration of Sr.
 	call boundary_conds_conc(phi_Dom1,Conc_Dom1,Conc_Dom2)	
 
 	
 	!!Calculate chem. pot. for the concentration parameter
-	call calculate_potentials(phi_Dom1, Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
+	call calculate_potentials(phi_Dom1,Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
 	
 	!!Apply  No-Flux BC for chemical potential for the Cahn-Hilliard eqn.
-	call boundary_conds_potentials(Conc_Dom2, Pot_Dom1,Pot_Dom2)
+	call boundary_conds_potentials(Conc_Dom2,Pot_Dom1,Pot_Dom2)
 	
 	!! Iterate the Cahn-Hilliard Equation
 	call Iterations(Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
 		
 	if (mod(iter,it_mod) .eq. 0) then	
-		call write_output(Conc_Dom1,Conc_Dom2,iter)
+		write(*,*) 'total phi=',sum(phi_Dom1)
+		call write_output(Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2,iter)
 	endif
 
 enddo
@@ -76,7 +77,6 @@ implicit none
 	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Conc_Dom2
 	integer :: i,j	
 
-	
 	!!No-Flux BC 
 	Conc_Dom1(0,:)=Conc_Dom1(1,:)
 	Conc_Dom1(nx+1,:)=Conc_Dom1(nx,:)
@@ -95,15 +95,15 @@ implicit none
 			Conc_Dom2(i,ny/2)=Conc_Dom1(i,ny/2)
 			phi_Dom1(i,ny/2+1)=1.d0
 		else
-		Conc_Dom1(i,ny/2+1)=Conc_Dom1(i,ny)	
+		Conc_Dom1(i,ny/2+1)=Conc_Dom1(i,ny/2)	
 		Conc_Dom2(i,ny/2)=Conc_Dom2(i,ny/2+1)
 		endif		
 	enddo
-	
+
 end subroutine
 !*********************************************************************
 !*********************************************************************
-subroutine calculate_potentials(phi_Dom1, Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
+subroutine calculate_potentials(phi_Dom1,Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
 use simulation
 implicit none
 ! Impose periodic boundary conditions
@@ -122,24 +122,32 @@ implicit none
 
 	!For Domain2
 	!First derivative of the bulk free e.g. density
-	dgdc=.5d0*W*Conc_Dom2*(1.0d0-Conc_Dom2)*(1.0d0-2.0d0*Conc_Dom2)	
+	dgdc=.5d0*W*(Conc_Dom2-CmBt)*Conc_Dom2**2.d0+.5d0*W*Conc_Dom2*(Conc_Dom2-CmBt)**2.d0
+
+	forall(i=1:nx,j=ny/2+1:ny/2+1)
+ 		Pot_Dom2(i,j)=dgdc(i,j)-eps2*((Conc_Dom2(i+1,j)+Conc_Dom2(i-1,j)-2.d0*Conc_Dom2(i,j))/(dx*dx)) &
+ 			+0.01d0*eps2*((Conc_Dom2(i,j+1)+Conc_Dom2(i,j-1)-2.d0*Conc_Dom2(i,j))/(dy*dy))
+   	end forall
 	
-	forall(i=1:nx,j=ny/2+1:ny)
+	forall(i=1:nx,j=ny/2+2:ny)
  		Pot_Dom2(i,j)=dgdc(i,j)-eps2*((Conc_Dom2(i+1,j)+Conc_Dom2(i-1,j)-2.d0*Conc_Dom2(i,j))/(dx*dx) &
  			+(Conc_Dom2(i,j+1)+Conc_Dom2(i,j-1)-2.d0*Conc_Dom2(i,j))/(dy*dy))
    	end forall
+	
+
 
 end subroutine
 !*********************************************************************
 !*********************************************************************
-subroutine boundary_conds_potentials(Conc_Dom2, Pot_Dom1,Pot_Dom2)	
+subroutine boundary_conds_potentials(Conc_Dom2,Pot_Dom1,Pot_Dom2)	
 use simulation
 implicit none
 ! Impose periodic boundary conditions
-	real(kind=DBL), DIMENSION(0:nx+1,0:ny/2+1) :: Pot_Dom1, Conc_Dom2
-	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Pot_Dom2
+	real(kind=DBL), DIMENSION(0:nx+1,0:ny/2+1) :: Pot_Dom1 
+	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Conc_Dom2, Pot_Dom2
 	integer :: i,j	
 	
+
 	!!No-Flux BC 
 	Pot_Dom2(0,:)=Pot_Dom2(1,:)
 	Pot_Dom2(nx+1,:)=Pot_Dom2(nx,:)
@@ -149,11 +157,17 @@ implicit none
 	!Non-zero flux at the interface between substrate and particle
 	do i=1,nx
 		if (Conc_Dom2(i,ny/2+1) .gt. 0.5) then
+			Pot_Dom1(i,ny/2+1)=Pot_Dom2(i,ny/2+1)
 			Pot_Dom2(i,ny/2)=Pot_Dom1(i,ny/2)
 		else
+		Pot_Dom1(i,ny/2+1)=Pot_Dom1(i,ny/2)
 		Pot_Dom2(i,ny/2)=Pot_Dom2(i,ny/2+1)
 		endif		
 	enddo
+
+! 	write(*,*) Pot_Dom1(nx/2,ny/2-1),Pot_Dom1(nx/2,ny/2),Pot_Dom1(nx/2,ny/2+1)
+! 	write(*,*) Pot_Dom2(nx/2,ny/2),Pot_Dom2(nx/2,ny/2+1),Pot_Dom2(nx/2,ny/2+2)
+! 	stop
 	
 end subroutine
 !*********************************************************************
@@ -162,13 +176,14 @@ subroutine Iterations(Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2)
 use simulation
 implicit none
 ! Impose periodic boundary conditions
+	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1) :: Conc
 	real(kind=DBL), DIMENSION(0:nx+1,0:ny/2+1) :: Conc_Dom1, Pot_Dom1, div_Dom1, Mob_Dom1
 	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Conc_Dom2, Pot_Dom2, div_Dom2, Mob_Dom2
 	integer :: i,j
 	
 	Mob_Dom1=1.d0
 	Mob_Dom2=1.d0
-	
+		
 	!For Domain 1
 	forall(i=1:nx,j=1:ny/2)
     	div_Dom1(i,j)=((Mob_Dom1(i,j)+Mob_Dom1(i+1,j))*(Pot_Dom1(i+1,j)-Pot_Dom1(i,j))-(Mob_Dom1(i,j)+Mob_Dom1(i-1,j))*(Pot_Dom1(i,j)-Pot_Dom1(i-1,j))) / (2.d0*dx*dx) + &
@@ -182,7 +197,10 @@ implicit none
     			 ((Mob_Dom2(i,j)+Mob_Dom2(i,j+1))*(Pot_Dom2(i,j+1)-Pot_Dom2(i,j))-(Mob_Dom2(i,j)+Mob_Dom2(i,j-1))*(Pot_Dom2(i,j)-Pot_Dom2(i,j-1))) / (2.d0*dy*dy)
    	end forall
 	Conc_Dom2=Conc_Dom2+dt*div_Dom2
-	    
+
+! 	write(*,*) div_Dom1(nx/2,ny/2-1),div_Dom1(nx/2,ny/2)
+! 	write(*,*) div_Dom2(nx/2,ny/2+1),div_Dom2(nx/2,ny/2+2)
+! 	stop   
 end subroutine
 !*********************************************************************
 !*********************************************************************
@@ -221,17 +239,17 @@ implicit none
 	enddo	
 
 	Conc_Dom1(:,1:ny/2)=Conc(:,1:ny/2)
-	Conc_Dom2(:,ny/2+1:ny)=Conc(:,ny/2+1:ny)
+	Conc_Dom2(:,ny/2+1:ny)=Conc(:,ny/2+1:ny)	
 		 	
 end subroutine
 !*********************************************************************
 !********************************************************************* 	
-subroutine write_output(Conc_Dom1,Conc_Dom2,iter)
+subroutine write_output(Conc_Dom1,Conc_Dom2,Pot_Dom1,Pot_Dom2,iter)
 use simulation
 implicit none
 	real(kind=DBL), DIMENSION(0:nx+1,0:ny+1) :: Conc
-	real(kind=DBL), DIMENSION(0:nx+1,0:ny/2+1) :: Conc_Dom1
-	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Conc_Dom2
+	real(kind=DBL), DIMENSION(0:nx+1,0:ny/2+1) :: Conc_Dom1,Pot_Dom1
+	real(kind=DBL), DIMENSION(0:nx+1,ny/2:ny+1) :: Conc_Dom2,Pot_Dom2
 	INTEGER :: iter
 	CHARACTER(LEN=100) :: filename
 	CHARACTER(LEN=10) :: iteration
@@ -265,8 +283,10 @@ implicit none
 	
 	write(iteration,format_string)iter	
 
-	Conc(:,1:ny/2)=Conc_Dom1(:,1:ny/2)
-	Conc(:,ny/2+1:ny)=Conc_Dom2(:,ny/2+1:ny)
+	Conc=0.0d0
+
+ 	Conc(:,1:ny/2)=Conc_Dom1(:,1:ny/2)
+	Conc(:,ny/2:ny)=Conc_Dom2(:,ny/2:ny)
 	
 	filename='data/'//trim(s)//'/'//trim(dates)//'/'//trim(s)//'_Conc_t'//trim(iteration)//'_'//trim(dates)//'.dat'
 	write(*,*) filename
@@ -274,6 +294,15 @@ implicit none
 	write(1) Conc(1:nx,1:ny)
 	close(1)	
 
+	Conc=0.0d0
+ 	Conc(:,1:ny/2)=Pot_Dom1(:,1:ny/2)
+	Conc(:,ny/2:ny)=Pot_Dom2(:,ny/2:ny)
+
+	filename='data/'//trim(s)//'/'//trim(dates)//'/'//trim(s)//'_ChemPot_t'//trim(iteration)//'_'//trim(dates)//'.dat'
+	write(*,*) filename
+	open(1,file=filename,form='unformatted',STATUS='REPLACE',ACTION='READWRITE')
+	write(1) Conc(1:nx,1:ny)
+	close(1)
 	
 end subroutine
 !*********************************************************************
