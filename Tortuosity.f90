317337b626425a3a4b0bdc11497c60a8391611ec
challan <challan@umich.edu>
2017-06-01 21:04:23 -0400

    Updated "Tortuosity.f90"
    Updated the code such that 
    
    This code updated to select the minimum distance value
    among 13 nearest neighboring points in the previous + current jth plane. The multiple loop
    over the ith and kth direction ensures that all possible path ways are considered.

--- Tortuosity.f90
+++ Tortuosity.f90
@@ -17,6 +17,10 @@
 ! This takes care of scenarios where the distance values might be different depending on 
 ! the distance value of the neighboring grid points at the previous plane. 
 
+! Modified by Chal Park June 1st 2017 - This code updated to select the minimum distance value
+! among 13 nearest neighboring points in the previous + current jth plane. The multiple loop
+! over the ith and kth direction ensures that all possible path ways are considered.
+
 module simulation
 implicit none
 
@@ -32,23 +36,29 @@ use simulation
 Implicit None
 !......................................................................................... 
 Integer:: phi(1:px,1:py,1:pz)
-Real:: DIST1(1:px,1:py,1:pz)
-Real:: DIST2(1:px,1:py,1:pz)
+Real:: DIST(1:px,1:py,1:pz)
 
 phi(:,:,:)=0
-
-phi(3,1:py/2-3,5)=1
-phi(4,1:py/2-2,5)=1
-
+! phi(5,1:py,5)=1
+! phi(4,5:py,5)=1
+! phi(3,py,5)=1
+!! Initial Condition 1
+phi(3,1:2,5)=1
+phi(4,1:3,5)=1
 phi(5,:,5)=1
-
 phi(4,5:py,4:5)=1
 phi(3,6:py,4:5)=1
 phi(5,py,4)=1
-DIST1(:,:,:) = 0.d0	
-DIST2(:,:,:) = 0.d0
+
+!! Initial Condition 2
+! phi(2,1:3,5)=1
+! phi(2:5,4,5)=1
+! phi(5,5:py,5)=1
+
+DIST(:,:,:) = 0.d0	
+
 	!call the subroutine to calculate tortuosity
-	call quasi_euclidian(phi,DIST1,DIST2)
+	call quasi_euclidian(phi,DIST)
 
 End Program Tortuosity
 !   ____                  _   ______           _ _     _ _             
@@ -60,179 +70,351 @@ End Program Tortuosity
 !This method flags each location based on adjacency as well as edge sharing.
 !Corner blocks are not considered in each iteration, but are eventually flagged when an adjacent/edge block is found.
 !Tortuosity will be lower than city block.
-SUBROUTINE quasi_euclidian(phi,DIST1,DIST2)
+SUBROUTINE quasi_euclidian(phi,dist)
 use simulation
 Implicit None
 Integer :: i,j,k,l,latdir_check,count_exch													!Iterating integers
 Real :: marker,counter,ydir,xdir,zdir														!Counters and markers
 Integer:: phi(1:px,1:py,1:pz)
-Real:: DIST1(1:px,1:py,1:pz),DIST2(1:px,1:py,1:pz)
-                                                                                                              
-	ydir = 0.d0 
-																																					
-	Print*, 'Initialized first plane'
+Real:: dist(1:px,1:py,1:pz)
 
-	Do j = 2,2
-		Do i = 1,px; Do k = 1,pz
-			If(i .NE. 1 .AND. i .NE. px .AND. k .NE. 1 .AND. k .NE. pz) Then		
-				If(phi(i,j-1,k) == 1 .AND. phi(i,j,k) == 1) Then 							
-					ydir = DIST1(i,j-1,k) + 1													
-					DIST1(i,j,k) = ydir															
-				EndIf
-			EndIf				
-		EndDo; EndDo
-	EndDo	
-
-	DIST2=DIST1
-	Print*, 'Seeded first distance plane'
-
-!! Since the tortuosity value at the last plane changes depending on the order of the loop, we 
-!! repeat the same quasi-euclidian iteration but with different order of loop.	
-	Do j = 3,py
-		Do i = 1,px; Do k = 1,pz
-			If(i .NE. 1 .AND. i .NE. px .AND. k .NE. 1 .AND. k .NE. pz) Then			
-				If(phi(i,j-1,k) == 1 .AND. phi(i,j,k) == 1 .AND. DIST1(i,j-1,k) /= 0) Then 	
-					ydir = DIST1(i,j-1,k) + 1															
-					DIST1(i,j,k) = ydir																			
-						If(phi(i+1,j,k) == 1 .AND. DIST1(i+1,j,k) == 0) Then 					!Enter this if loop if the solid is actually connected
-							ydir = DIST1(i,j-1,k) + sqrt(2.0d0)									!This loop checks the new flag and its 4 adjacent points (edge to previous)						
-							DIST1(i+1,j,k) = ydir															
-						EndIf
-						If(phi(i-1,j,k) == 1 .AND. DIST1(i-1,j,k) == 0) Then 	
-							ydir = DIST1(i,j-1,k) + sqrt(2.0d0)														
-							DIST1(i-1,j,k) = ydir														
-						EndIf	
-						If(phi(i,j,k+1) == 1 .AND. DIST1(i,j,k+1) == 0) Then 	
-							ydir = DIST1(i,j-1,k) + sqrt(2.0d0)											
-							DIST1(i,j,k+1) = ydir														
-						EndIf					
-						If(phi(i,j,k-1) == 1 .AND. DIST1(i,j,k-1) == 0) Then 	
-							ydir = DIST1(i,j-1,k) + sqrt(2.0d0)										
-							DIST1(i,j,k-1) = ydir													
-						EndIf				
-				EndIf
-			EndIf					
-		EndDo; EndDo
-		
-		Do latdir_check = 1,100 																
-			Do i = px,1,-1; Do k = 1,pz
-				If(i .NE. 1 .AND. i .NE. px .AND. k .NE. 1 .AND. k .NE. pz) Then		
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i+1,j,k) == 0 .AND. phi(i+1,j,k) == 1) Then
-						DIST1(i+1,j,k) = DIST1(i,j,k) + 1
-					EndIf
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i-1,j,k) == 0 .AND. phi(i-1,j,k) == 1) Then
-						DIST1(i-1,j,k) = DIST1(i,j,k) + 1
-					EndIf	
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i,j,k+1) == 0 .AND. phi(i,j,k+1) == 1) Then
-						DIST1(i,j,k+1) = DIST1(i,j,k) + 1
-					EndIf	
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i,j,k-1) == 0 .AND. phi(i,j,k-1) == 1) Then
-						DIST1(i,j,k-1) = DIST1(i,j,k) + 1
-					EndIf	
-
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i+1,j,k+1) == 0 .AND. phi(i+1,j,k+1) == 1) Then
-						DIST1(i+1,j,k+1) = DIST1(i,j,k) + sqrt(2.0d0)
-					EndIf
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i-1,j,k-1) == 0 .AND. phi(i-1,j,k-1) == 1) Then
-						DIST1(i-1,j,k-1) = DIST1(i,j,k) + sqrt(2.0d0)
-					EndIf	
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i-1,j,k+1) == 0 .AND. phi(i-1,j,k+1) == 1) Then
-						DIST1(i-1,j,k+1) = DIST1(i,j,k) + sqrt(2.0d0)
-					EndIf	
-					If(DIST1(i,j,k) /= 0 .AND. DIST1(i+1,j,k-1) == 0 .AND. phi(i+1,j,k-1) == 1) Then
-						DIST1(i+1,j,k-1) = DIST1(i,j,k) + sqrt(2.0d0)
-					EndIf		
-				EndIf
-					
-			EndDo; EndDo
-		EndDo	
-		Print*, 'Checked connection to adjacent locations', j
-
+!!The multiple do loops in the ith and kth direction ensures that the minimum
+!! distance value is selected regardless of the direction of the tortuous path. 
+	Do j=2,2
+		Do i=2,px-1
+			Do k=2,pz-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator1(dist,phi,i,j,k)
+				endif
+			EndDo
+			Do k=pz-1,2,-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator1(dist,phi,i,j,k)
+				endif
+			EndDo			
+		EndDo
+		Do i=px-1,2,-1
+			Do k=2,pz-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator1(dist,phi,i,j,k)
+				endif
+			EndDo
+			Do k=pz-1,2,-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator1(dist,phi,i,j,k)
+				endif
+			EndDo			
+		EndDo
 	EndDo
-
-	Do j = 3,py
-		Do i = px,1,-1; Do k = 1,pz
-			If(i .NE. 1 .AND. i .NE. px .AND. k .NE. 1 .AND. k .NE. pz) Then			
-				If(phi(i,j-1,k) == 1 .AND. phi(i,j,k) == 1 .AND. DIST2(i,j-1,k) /= 0) Then 	
-					ydir = DIST2(i,j-1,k) + 1															
-					DIST2(i,j,k) = ydir																			
-						If(phi(i+1,j,k) == 1 .AND. DIST2(i+1,j,k) == 0) Then 					!Enter this if loop if the solid is actually connected
-							ydir = DIST2(i,j-1,k) + sqrt(2.0d0)									!This loop checks the new flag and its 4 adjacent points (edge to previous)						
-							DIST2(i+1,j,k) = ydir															
-						EndIf
-						If(phi(i-1,j,k) == 1 .AND. DIST2(i-1,j,k) == 0) Then 	
-							ydir = DIST2(i,j-1,k) + sqrt(2.0d0)														
-							DIST2(i-1,j,k) = ydir														
-						EndIf	
-						If(phi(i,j,k+1) == 1 .AND. DIST2(i,j,k+1) == 0) Then 	
-							ydir = DIST2(i,j-1,k) + sqrt(2.0d0)											
-							DIST2(i,j,k+1) = ydir														
-						EndIf					
-						If(phi(i,j,k-1) == 1 .AND. DIST2(i,j,k-1) == 0) Then 	
-							ydir = DIST2(i,j-1,k) + sqrt(2.0d0)										
-							DIST2(i,j,k-1) = ydir													
-						EndIf				
-				EndIf
-			EndIf					
-		EndDo; EndDo
-		
-		Do latdir_check = 1,100 																
-			Do i = px,1,-1; Do k = 1,pz
-				If(i .NE. 1 .AND. i .NE. px .AND. k .NE. 1 .AND. k .NE. pz) Then		
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i+1,j,k) == 0 .AND. phi(i+1,j,k) == 1) Then
-						DIST2(i+1,j,k) = DIST2(i,j,k) + 1
-					EndIf
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i-1,j,k) == 0 .AND. phi(i-1,j,k) == 1) Then
-						DIST2(i-1,j,k) = DIST2(i,j,k) + 1
-					EndIf	
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i,j,k+1) == 0 .AND. phi(i,j,k+1) == 1) Then
-						DIST2(i,j,k+1) = DIST2(i,j,k) + 1
-					EndIf	
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i,j,k-1) == 0 .AND. phi(i,j,k-1) == 1) Then
-						DIST2(i,j,k-1) = DIST2(i,j,k) + 1
-					EndIf	
-
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i+1,j,k+1) == 0 .AND. phi(i+1,j,k+1) == 1) Then
-						DIST2(i+1,j,k+1) = DIST2(i,j,k) + sqrt(2.0d0)
-					EndIf
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i-1,j,k-1) == 0 .AND. phi(i-1,j,k-1) == 1) Then
-						DIST2(i-1,j,k-1) = DIST2(i,j,k) + sqrt(2.0d0)
-					EndIf	
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i-1,j,k+1) == 0 .AND. phi(i-1,j,k+1) == 1) Then
-						DIST2(i-1,j,k+1) = DIST2(i,j,k) + sqrt(2.0d0)
-					EndIf	
-					If(DIST2(i,j,k) /= 0 .AND. DIST2(i+1,j,k-1) == 0 .AND. phi(i+1,j,k-1) == 1) Then
-						DIST2(i+1,j,k-1) = DIST2(i,j,k) + sqrt(2.0d0)
-					EndIf		
-				EndIf
-					
-			EndDo; EndDo
-		EndDo	
-		Print*, 'Checked connection to adjacent locations', j
-
+ 
+	Do j=3,py
+		Do i=2,px-1
+			Do k=2,pz-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator2(dist,i,j,k)
+				endif
+			EndDo
+			Do k=pz-1,2,-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator2(dist,i,j,k)
+				endif
+			EndDo			
+		EndDo
+		Do i=px-1,2,-1
+			Do k=2,pz-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator2(dist,i,j,k)
+				endif
+			EndDo
+			Do k=pz-1,2,-1
+				if (phi(i,j,k) .eq. 1) then
+					call distance_calculator2(dist,i,j,k)
+				endif
+			EndDo			
+		EndDo
 	EndDo
-
+	
 ! counter => no. of grid points with non-zero distance value at the last plane
-! count_exch => no. of grid points with unequal distance values based on different loop direction	
 	counter = 0
-	count_exch = 0
-	Do i = 1,px; Do j = py,py; Do k = 1,pz
-		If(DIST1(i,j,k) /= 0) Then
+	Do i = 2,px-1; Do k = 2,pz-1
+		If(DIST(i,py,k) /= 0) Then
 			counter = counter + 1
-		EndIf
-		If(DIST2(i,j,k) .lt. DIST1(i,j,k)) Then
-			DIST1(i,j,k) = DIST2(i,j,k)
-			count_exch = count_exch +1
-		EndIf		
-	EndDo; EndDo; EndDo
+		EndIf	
+	EndDo; EndDo
 
-	print*,DIST1(5,py,5),DIST1(4,py,5),DIST1(3,py,5)
-	print*,DIST1(5,py,4),DIST1(4,py,4),DIST1(3,py,4)
+	print*,DIST(5,py,5),DIST(4,py,5),DIST(3,py,5)
+	print*,DIST(5,py,4),DIST(4,py,4),DIST(3,py,4)
 
-	print*, 'exchanged distance values: ', count_exch
 	print*, 'counter: ',counter
-	print*, 'Tortuosity: ', (dble(sum(DIST1(1:px,py:py,1:pz)))/(dble(counter)))/(dble(py))
+
+	print*, 'Tortuosity: ', (dble(sum(DIST(2:px-1,py,2:pz-1)))/(dble(counter)))/(dble(py))
 
 END SUBROUTINE
-	
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SUBROUTINE	distance_calculator1(dist,phi,i,j,k)
+use simulation
+Implicit None
+Integer :: i,j,k,phi(1:px,1:py,1:pz)
+Real:: new_dist,dist(1:px,1:py,1:pz)	
+!! For grid points in the second plane (especially case)
+!! Finds the distance value at a grid point within the phase = 1	
+!! Calculates the accumulated distance based on the predetermined distance values at 13 
+!! nearest neighbors in the previous plane and in the current plane.
+!! The smallest non-zero distance value is assigned.
+!! The subroutine takes into account situations where the grid point is surrounded the other phase
+!! in the previous and current planes (in this case it will assign the distance of 0).
 
+if(phi(i,j-1,k) .eq. 1) then
+	new_dist=dist(i,j-1,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif
+if(phi(i-1,j-1,k) .eq. 1) then
+	new_dist=dist(i-1,j-1,k)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif			
+if(phi(i+1,j-1,k) .eq. 1) then
+	new_dist=dist(i+1,j-1,k)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0	
+endif			
+if(phi(i,j-1,k-1) .eq. 1) then
+	new_dist=dist(i,j-1,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0	
+endif				
+if(phi(i,j-1,k+1) .eq. 1) then
+	new_dist=dist(i,j-1,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0	
+endif		
+!! Calculate distances from the nearest neighbors within the same plane
+if(dist(i-1,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif
+if(dist(i-1,j,k) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif
+if(dist(i-1,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif				
+if(dist(i,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i,j,k-1)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif	
+if(dist(i,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i,j,k+1)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif				
+if(dist(i+1,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif	
+if(dist(i+1,j,k) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif
+if(dist(i+1,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	new_dist=0.d0
+endif
+END SUBROUTINE
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SUBROUTINE	distance_calculator2(dist,i,j,k)
+use simulation
+Implicit None
+Integer :: i,j,k
+Real:: new_dist, dist(1:px,1:py,1:pz)
+!! Finds the distance value at a grid point within the phase = 1	
+!! Calculates the accumulated distance based on the predetermined distance values at 13 
+!! nearest neighbors in the previous plane and in the current plane.
+!! The smallest non-zero distance value is assigned.
+!! The subroutine takes into account situations where the grid point is surrounded the other phase
+!! in the previous and current planes (in this case it will assign the distance of 0).
+
+new_dist=0.d0
+
+if(dist(i,j-1,k) .ne. 0.d0) then
+	new_dist=dist(i,j-1,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here1'	
+endif
+if(dist(i-1,j-1,k) .ne. 0.d0) then
+	new_dist=dist(i-1,j-1,k)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here2'
+endif			
+if(dist(i+1,j-1,k) .ne. 0.d0) then
+	new_dist=dist(i+1,j-1,k)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here3'	
+endif			
+if(dist(i,j-1,k-1) .ne. 0.d0) then
+	new_dist=dist(i,j-1,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here4'
+endif				
+if(dist(i,j-1,k+1) .ne. 0.d0) then
+	new_dist=dist(i,j-1,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here5'	
+endif		
+!! Calculate distances from the nearest neighbors within the same plane
+if(dist(i-1,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here6'
+endif
+if(dist(i-1,j,k) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here7'
+endif
+if(dist(i-1,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i-1,j,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here8'
+endif				
+if(dist(i,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i,j,k-1)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here9'
+endif	
+if(dist(i,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i,j,k+1)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here10'
+endif
+				
+if(dist(i+1,j,k-1) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k-1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here11'
+endif	
+if(dist(i+1,j,k) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k)+1.d0
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here12'
+endif
+if(dist(i+1,j,k+1) .ne. 0.d0) then
+	new_dist=dist(i+1,j,k+1)+sqrt(2.d0)
+		if (new_dist .lt. dist(i,j,k)) then
+			dist(i,j,k)=new_dist
+		elseif (dist(i,j,k) .eq. 0.d0) then
+			dist(i,j,k)=new_dist
+		endif
+	print*,i,j,k,dist(i,j,k),'here13'
+endif
+END SUBROUTINE
